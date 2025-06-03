b66384c15f404374adaf2353fe776071d06a624a
NiranjanaaR <99683589+NiranjanaaR@users.noreply.github.com>
2025-06-03 16:27:19 +0200

    comit
    commit

--- SegAnyGAussians_final/segmentation_res/chk.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-mask = torch.load("precomputed_mask.pt", map_location="cpu")
-
-print("Type:", type(mask))
-if torch.is_tensor(mask):
-    print("Dtype:", mask.dtype)
-    print("Shape:", mask.shape)
-    print("Unique values:", torch.unique(mask))
